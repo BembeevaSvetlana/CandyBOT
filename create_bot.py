ae4b45354e0ac287061871051399245812903bb0
SvetlanaB <kapk2004@inbox.ru>
2022-11-15 18:48:14 +0300

    CreateBot

---
+++ create_bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher
+
+
+bot = Bot(token='5654213325:AAHvH-fv17ZT92AhMAQl-6HUGUI4s0JP7Qg')
+#bot = Bot(token='5392517954:AAGDqXzumb4MNkium4tw4U6jP_15svw_Wt8')
+dp = Dispatcher(bot)
