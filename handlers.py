f2cb972e14848d2d57a6e6a04ecd76137005ee0a
SvetlanaB <kapk2004@inbox.ru>
2022-11-18 19:39:14 +0300

    second

--- handlers.py
+++ handlers.py
@@ -7,3 +7,8 @@ def registred_handlers(dp: Dispatcher):
     dp.register_message_handler(commands.start, commands=['start'])
     dp.register_message_handler(commands.set_total_candies, commands=['set'])
     # dp.register_message_handler(commands.player_turn)
+    #dp.register_message_handler(commands.finish, commands=['end'])
+    dp.register_message_handler(commands.max_take, commands=['take'])
+  
+    dp.register_message_handler(commands.level, commands=['level'])
+   
