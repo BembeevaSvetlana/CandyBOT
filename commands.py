3d5bfb12bd9d4552ae598602146887053fbc9bd4
SvetlanaB <kapk2004@inbox.ru>
2022-11-18 19:38:31 +0300

    some changes

--- commands.py
+++ commands.py
@@ -9,14 +9,34 @@ import model
 from create_bot import bot
 
 
+
 async def start(message: types.Message):
-    print(message.text)
-    print(message.from_user.id)
-    print(message.from_user.first_name)
-    player = message.from_user
-    model.set_player(player)
+    
     await view.hello(message)
     await asyncio.sleep(3)
+    await view.choice(message)
+    await view.choice2(message)
+    await view.choice3(message)
+     
+async def level(message:types.Message):
+    level=int((message.text).split(" ")[1])
+    model.set_level(level)
+   
+    await bot.send_message(message.from_user.id, f'выбран уровень' f' {level}'f':0-это скучный уровень, 1-веселый уровень')
+    
+async def set_total_candies(message: types.Message):
+
+    count = int((message.text).split(" ")[1])
+    model.set_total_candies(count)
+    await bot.send_message(message.from_user.id, f'Максимальное количество конфет 'f' {count}')
+    
+ 
+async def max_take(message:types.Message):
+    player = message.from_user
+    model.set_player(player)
+    max_take=int((message.text).split(" ")[1])
+    model.set_max_take(max_take)
+    await bot.send_message(message.from_user.id, f'Максимально количество конфет,которое можно взять за раз,'f' {max_take}')
     dp.register_message_handler(player_turn)
     first_turn = random.randint(0,1)
     if first_turn:
@@ -27,13 +47,13 @@ async def start(message: types.Message):
 async def player_turn(message: types.Message):
     player = message.from_user
     model.set_player(player)
+    number = model.get_max_take()
     if (message.text).isdigit():
-        if 0 < int(message.text) < 29:
+        if 0 < int(message.text) < number:
             total_count = model.get_total_candies()
             player_take = int(message.text)
             total = total_count - player_take
-            await bot.send_message(player.id, f'{player.first_name} взял {player_take} конфет, '
-                                              f'и на столе осталось {total}')
+            await bot.send_message(player.id, f'{player.first_name} взял {player_take} конфет, 'f'и на столе осталось {total}')
             if model.check_win(total):
                 await bot.send_message(player.id, f'Победил {player.first_name}')
                 return
@@ -43,43 +63,42 @@ async def player_turn(message: types.Message):
         else:
             await bot.send_message(message.from_user.id, 'А не многовато ли взял')
     else:
-        await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, '
-                                                     f'вообще-то мы конфеты считаем в цифрах')
+        await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, ' f'вообще-то мы конфеты считаем в цифрах')
 
 async def enemy_turn(player):
     total_count = model.get_total_candies()
-    if total_count < 29:
-        enemy_take = total_count
+    number= model.get_max_take()
+    lev=model.get_level()
+    if lev == 1:
+        if total_count < number:
+            enemy_take = total_count
+        else:
+            enemy_take = (total_count - 1) % number
+            total = total_count - enemy_take
+            await bot.send_message(player.id, f'Бот взял {enemy_take} конфет, 'f'и на столе осталось {total}')
+        if model.check_win(total):
+            await bot.send_message(player.id, f'{player.first_name} ты проиграл,'f'тебя дёрнула железяка')
+            return
+        model.set_total_candies(total)
+        await asyncio.sleep(1)
+        await await_player(player)
+
+            
     else:
-        enemy_take = (total_count - 1)%28
-    total = total_count - enemy_take
-    await bot.send_message(player.id, f'Бот взял {enemy_take} конфет, '
-                                      f'и на столе осталось {total}')
-    if model.check_win(total):
-        await bot.send_message(player.id, f'{player.first_name} ты проиграл,'
-                                          f'тебя дёрнула железяка')
-        return
-    model.set_total_candies(total)
-    await asyncio.sleep(1)
-    await await_player(player)
+            # total_count = model.get_total_candies()
+            # number = model.get_max_take()
+        enemy_take = random.randint(1, number)
+        total = total_count - enemy_take
+        await bot.send_message(player.id, f'Бот взял {enemy_take} конфет, 'f'и на столе осталось {total}')
+        if model.check_win(total):
+            await bot.send_message(player.id, f'{player.first_name} ты проиграл,'f'тебя дёрнула железяка')
+            return 
+        model.set_total_candies(total)
+        await asyncio.sleep(1)
+        await await_player(player)
 
-
-
-
+    
 async def await_player(player):
     max_take = model.get_max_take()
-    await bot.send_message(player.id,
-                           f'{player.first_name}, бери конфеты, но не больше {max_take}')
+    await bot.send_message(player.id, f'{player.first_name}, бери конфеты, но не больше {max_take}')
 
-async def stop(message: types.Message):
-    await bot.send_message(message.from_user.id,
-                           f'{message.from_user.first_name}, чё доигрался?')
-async def set_total_candies(message: types.Message):
-
-async def anything(message: types.Message):
-    await bot.send_message(message.from_user.id,
-                           f'{message.text} - это еще что за хрень? ')
-    count = int((message.text).split(" ")[1])
-    model.set_total_candies(count)
-    await bot.send_message(message.from_user.id, f'Максимально количество конфет изменили на'
-                                                 f' {count}')
